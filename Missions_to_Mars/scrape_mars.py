6c91e442fea867264352598ce4fd3cac4fdccbf5
tawnynichols <65970072+tawnynichols@users.noreply.github.com>
2020-11-16 22:11:40 -0800

    fixed bug for mars data

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from splinter import Browser
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
@@ -13,50 +14,59 @@ def init_browser():
 # Scraping Script
 ############################################################### 
 
-def scrape():
+#Create empty dictionary to store values
+mars_data = {}
 
+def scrape():
+    # Run init_browser
     browser = init_browser()
 
     ###############################################################
     # Mars News Article
     ############################################################### 
+
     # set url for browser
     news_domain = 'https://mars.nasa.gov'
     url = news_domain + '/news/'
     browser.visit(url)
 
-    # Get news_title and news_p
     # HTML object
     html = browser.html
     # Parse HTML with Beautiful Soup
     soup = BeautifulSoup(html, 'html.parser')     
 
     # Use Beautiful Soup's find() method to navigate and retrieve attributes
-    news_title  = soup.find('div', class_="content_title").text
-    news_p = soup.find('div', class_='article_teaser_body').text
-    img_header =soup.find('div', class_='list_image')
-    img = img_header.find('img')['src']
-    news_image = 'https://mars.nasa.gov' + img
-    print("Mars News Scraping Complete")
+    article = soup.find("div", class_="list_text")
+    news_title = article.find("div", class_="content_title").text
+    news_p = article.find("div", class_="article_teaser_body").text
 
     # Store data in a dictionary
-    mars_data = {
-        "news_title": news_title,
-        "news_p": news_p,
-        "news_image": news_image,
-        "news_domain" : url
+    mars_data["news_title"] = news_title
+    mars_data["news_p"] = news_p
+    mars_data["news_domain"] = news_domain
+    mars_data["news"] = url
 
+    # Use Beautiful Soup's find() method to navigate and retrieve attributes
+    img_header =soup.find('div', class_='list_image')
+    img = img_header.find('img')['src']
+    news_image = news_domain + img
 
-    } 
+    # Store data in a dictionary
+    mars_data["news_image"] = news_image
+    print("Mars News Scraping Complete")   
 
+    # Close the browser after scraping
+    browser.quit()
     
     ###############################################################
     # Featured Image
     ############################################################### 
-    
+    # Run init_browser
+    browser = init_browser()
+
     # set url for browser
-    Featured_domain = 'https://www.jpl.nasa.gov'
-    space_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    Featured_domain = 'https://www.jpl.nasa.gov/'
+    space_url = Featured_domain + 'spaceimages/?search=&category=Mars'
     browser.visit(space_url)
 
     # Use splinter to navigate the site and find the image url for the current Featured Mars Image and assign the url string to a variable called featured_image_url.
@@ -92,20 +102,23 @@ def scrape():
                 featured_image_url  = Featured_domain + figure_url
                 mars_data["featured_image_url"] =  featured_image_url  
                 mars_data["Featured_domain"]  = Featured_domain
+                mars_data["space_url"]  = space_url
                 print("Featured Image Complete")  
             
         except:
             print("Featured Image Scraping Error")  
 
+    # Close the browser after scraping
+    browser.quit()
+
     ###############################################################
     # Mars  Facts
     ############################################################### 
+    # Run init_browser
+    browser = init_browser()
 
     # Visit the Mars Facts webpage here 
     mars_url = 'https://space-facts.com/mars/'
-
-    # use Pandas to scrape the table containing facts about the planet including Diameter, Mass, etc.
-    import pandas as pd
 
     # Use Pandas to convert the data to a HTML table string.
     tables = pd.read_html(mars_url)
@@ -122,10 +135,14 @@ def scrape():
     mars_data["table"] =  table    
     print("Mars Facts Complete")
 
+    # Close the browser after scraping
+    browser.quit()
 
     ###############################################################
     # Mars  Hemispheres
     ############################################################### 
+    # Run init_browser
+    browser = init_browser()
 
     # Visit the USGS Astrogeology site here to obtain high resolution images for each of Mar's hemispheres.
     usgs_site = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
@@ -188,6 +205,8 @@ def scrape():
 
     # Close the browser after scraping
     browser.quit()
+
+    print("All Scraping Complete!")
 
     # Return results
     return mars_data  
